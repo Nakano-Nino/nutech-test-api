075bf50dbded8adaabbcf8921aaefd707d9e8e68
Yudha Prastyo <yudhaprasty17@gmail.com>
2024-04-05 15:43:53 +0700

    small fix

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -200,12 +200,6 @@ export default new (class UserController {
       const { email } = res.locals.loginSession;
       type QueryResult = any[];
 
-      cloudinary.config({
-        cloud_name: process.env.CLOUD_NAME,
-        api_key: process.env.API_KEY,
-        api_secret: process.env.API_SECRET,
-      });
-
       let profile_image = "";
 
       if (req.file) {
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,19 @@
 import express from "express";
 import cors from "cors";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
+import { v2 as cloudinary } from "cloudinary";
 
 dotenv.config();
 
 import userRouter from "./routes/user.routes";
 import informationRouter from "./routes/information.routes";
 import transactionRouter from "./routes/transaction.routes";
+
+cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.API_KEY,
+    api_secret: process.env.API_SECRET,
+  });
 
 const app = express();
 const port = process.env.PORT || 3000
