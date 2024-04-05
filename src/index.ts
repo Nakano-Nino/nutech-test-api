de459de7a9554f9d1660dce547d0772e486aaff5
Yudha Prastyo <yudhaprasty17@gmail.com>
2024-04-05 14:39:50 +0700

    add cors

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 
 import userRouter from "./routes/user.routes";
 import informationRouter from "./routes/information.routes";
@@ -7,6 +8,11 @@ import transactionRouter from "./routes/transaction.routes";
 const app = express();
 
 app.use(express.json())
+app.use(cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+}))
 app.use('/api', userRouter)
 app.use("/api", informationRouter)
 app.use("/api", transactionRouter)
